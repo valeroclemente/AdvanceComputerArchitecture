e6f79af83d733dd3b0f58dfe4a9ab293f7297bd4
valeroclemente <maria.valero59@gmail.com>
2015-12-04 17:21:03 -0500

    Update FiltMPI.c

--- MPI_windowns_codes/FiltMPI.c
+++ MPI_windowns_codes/FiltMPI.c
@@ -2,7 +2,7 @@
 /************************** MPI IMPLEMENTATION ******************************************/
 /****************** IMPLEMENTATION SUITABLE FOR MICROSOFT WINDOWS ***********************/
 /*********************** Image Averaging Filter Benchmark *******************************/
-//FILES NEEDED: bmp file of 8 bits (The repository has a sample file)
+//FILES NEEDED: bmp file of 8 bits (The repository has a sample file).
 //Find files in: Sample Data/lady1000.bmp
 
 
